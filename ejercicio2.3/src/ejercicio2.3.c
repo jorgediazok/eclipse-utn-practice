dcf08e969a34299320ca302b020c3e597097948f
Jorge <jorgediazok@gmail.com>
2021-03-21 00:48:32 -0300

    first commit on exercise 2.3

---
+++ ejercicio2.3/src/ejercicio2.3.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : 3.c
+ Author      : jorgedv
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	setbuf(stdout, NULL);
+
+
+	return EXIT_SUCCESS;
+}
