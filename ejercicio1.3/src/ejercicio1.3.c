b33abc7f76c7958c038987600746bd7f92773e16
Jorge <jorgediazok@gmail.com>
2021-03-21 18:45:02 -0300

    updated ejercicio 1.3

--- ejercicio1.3/src/ejercicio1.3.c
+++ ejercicio1.3/src/ejercicio1.3.c
@@ -12,5 +12,44 @@
 #include <stdlib.h>
 
 int main(void) {
+
+	setbuf(stdout, NULL);
+
+	int i;
+	int numeroIngresado;
+	int numeroMayor;
+	int numeroMenor;
+	int numeroMedio;
+	int flagMayor = 0;
+	int flagMenor = 0;
+	int flagMedio = 0;
+
+
+	for(i=0; i < 3; i++){
+
+	printf("Ingrese un número:\n");
+	scanf("%d", &numeroIngresado);
+
+	if(flagMayor == 0 || numeroIngresado > numeroMayor){
+		numeroMayor = numeroIngresado;
+		flagMayor = 1;
+   }
+
+
+	if(flagMenor == 0 || numeroIngresado < numeroMenor){
+	 numeroMenor = numeroIngresado;
+		flagMenor = 1;
+	}
+
+   }
+
+	numeroMedio = numeroIngresado;
+
+
+	printf("El número mayor es %d\n", numeroMayor);
+	printf("El númerno menor es %d\n", numeroMenor);
+	printf("El número del medio es %d\n", numeroMedio);
+
+
 	return EXIT_SUCCESS;
 }
