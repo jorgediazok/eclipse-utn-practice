654015cccc0f5585ce76eb54f74fc2d08de36ee0
Jorge <jorgediazok@gmail.com>
2021-03-21 14:45:29 -0300

    created ejercicio 1.3

---
+++ ejercicio1.3/src/ejercicio1.3.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : 3.c
+ Author      : jorgedv
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	return EXIT_SUCCESS;
+}
