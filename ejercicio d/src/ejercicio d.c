e0ea7755661d8621a15159078ed3679ee75357ca
Jorge <jorgediazok@gmail.com>
2021-03-22 00:07:39 -0300

    subo los ejercicios c y d a desarrollar

---
+++ ejercicio d/src/ejercicio d.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : ejercicio.c
+ Author      : jorgedv
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+//d-Operadores Logicos
+//Aplicando el concepto de Operadores Lógicos con valores booleanos en el condicional:
+//Solicitar un número al usuario
+//Informar si el mismo es par o impar.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
