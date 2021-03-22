83eb217ca50dcdbb3b3336e4366972330c372362
Jorge <jorgediazok@gmail.com>
2021-03-22 15:27:45 -0300

    added ejercicios de funciones

--- ejercicios varios/src/ejercicios varios.c
+++ ejercicios varios/src/ejercicios varios.c
@@ -11,7 +11,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int suma();
+/*int suma();
+int num3 = 2; //Puedo agregar una variable global y se suma también;
+
+//Función main inicia la ejecución del programa
 int main(void) {
 	setbuf(stdout, NULL);
 	//Testeando una función de suma
@@ -28,10 +31,44 @@ int main(void) {
 	printf("El resultado es: %i\n", suma(num1,num2));
 
 	return EXIT_SUCCESS;
-}
+}//Fin de la función main
 
 
 int suma(int num1, int num2){
-	int suma = num1 + num2;
+	int suma = num1 + num2 + num3;
 	return suma;
+}*/
+
+//Función main inicia la ejecución del programa
+
+
+//2 Función de saludo
+/*void saludo(){
+	printf("Hola Mundo!");
+}
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	saludo();
+
+	return EXIT_SUCCESS;
+}*/
+
+//3 creando estructuras
+
+struct perro{
+		char nombre[30];
+		int edad;
+		float peso;
+	}
+perro1={"Chikorita", 10, 3.5},
+perro2={"Michelle", 4, 2.1};
+
+int main() {
+	setbuf(stdout, NULL);
+
+	printf("El peso de: %s es %.2f Kg y tiene %i años\n", perro2.nombre, perro2.peso, perro2.edad);
+
+	return EXIT_SUCCESS;
 }
