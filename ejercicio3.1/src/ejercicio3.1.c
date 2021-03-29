7e3dbf78e47f798fed1eb3c804798e713d1876de
Jorge <jorgediazok@gmail.com>
2021-03-29 17:22:46 -0300

    ejercicio 3.1 done

--- ejercicio3.1/src/ejercicio3.1.c
+++ ejercicio3.1/src/ejercicio3.1.c
@@ -14,6 +14,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int esNumeroPar(int num);
+
 int main(void) {
+
+	setbuf(stdout, NULL);
+
+	int num;
+	int esPar;
+
+	printf("Ingrese un número. Si devuelve 1 es par, si devuelve 0 es impar: \n");
+	scanf("%d", &num);
+
+	esPar = esNumeroPar(num);
+
+	printf("El número es: %d \n", esPar);
+
 	return EXIT_SUCCESS;
 }
+
+
+int esNumeroPar(int num){
+	if(num % 2 == 0){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
