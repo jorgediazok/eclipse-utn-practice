dd023b745851bfb5681a7be6ee6a80a5b12d18d2
Jorge <jorgediazok@gmail.com>
2021-03-29 02:10:24 -0300

    ejercicio 3,1 set

--- ejercicio 3.b/src/ejercicio 3.b.c
+++ ejercicio 3.b/src/ejercicio 3.b.c
@@ -11,12 +11,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int operacion(int numeroIngresado1, int numeroIngresado2);
+
 int main(void) {
 
 	setbuf(stdout, NULL);
 
 	int numeroIngresado1;
 	int numeroIngresado2;
+	int resultadoSuma;
 
 	printf("Ingrese un número: \n");
 	scanf("%d", &numeroIngresado1);
@@ -24,15 +27,33 @@ int main(void) {
 	printf("Ingrese otro número: \n");
 	scanf("%d", &numeroIngresado2);
 
-	while((numeroIngresado1 < 10 && numeroIngresado2 <10)   || (numeroIngresado1 > 100 && numeroIngresado2 > 100)){
-		printf("Por favor ingrese un número mayor a 10 y menor a 100");
+	while((numeroIngresado1 < 10 || numeroIngresado2 <10)   || (numeroIngresado1 > 100 || numeroIngresado2 > 100)){
+		printf("Ambos números deben ser mayores a 10 y menores a 100");
 		scanf("%d", &numeroIngresado1);
 		scanf("%d", &numeroIngresado2);
 	}
 
+	resultadoSuma = numeroIngresado1 + numeroIngresado2;
 
-	printf("Sus numeros ingresados son %d %d", numeroIngresado1, numeroIngresado2);
+	printf("La suma de los números es %d", resultadoSuma);
+
+
+	//valorConDescuento = numeroIngresado - realizarDescuento(numeroIngresado, DESCUENTO);
+
+	//	printf("Sus numeros ingresados son %d y %d", numeroIngresado1, numeroIngresado2);
 
 
 	return EXIT_SUCCESS;
 }
+
+
+int operacion(int numero1, int numero2){
+	int suma;
+	int resta;
+
+	suma = numero1 + numero2;
+	resta = numero1 - numero2;
+
+	return suma && resta;
+}
+
---
+++ ejercicio3.1/src/ejercicio3.1.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : 1.c
+ Author      : jorgedv
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+
+ Crear una función que permita determinar si un número es par o no.
+ La función retorna 1 en caso afirmativo y 0 en caso contrario. Probar en el main.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	return EXIT_SUCCESS;
+}
