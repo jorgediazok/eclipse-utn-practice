96fe0c24cdb183945e77416f305dac5936be1259
Jorge <jorgediazok@gmail.com>
2021-03-30 00:18:31 -0300

    ejercicio 3.2 set

---
+++ ejercicio3.2/src/ejercicio3.2.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : 2.c
+ Author      : jorgedv
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+
+ Crear una función que muestre por pantalla el número flotante que recibe como parámetro.
+
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	setbuf(stdout, NULL);
+
+
+	return EXIT_SUCCESS;
+}
