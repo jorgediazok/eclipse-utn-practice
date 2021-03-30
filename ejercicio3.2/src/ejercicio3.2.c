47ee81f47e81424183d819b0d5812300d58e0429
Jorge <jorgediazok@gmail.com>
2021-03-30 01:27:58 -0300

    ejercicio 3.2 terminado

--- ejercicio3.2/src/ejercicio3.2.c
+++ ejercicio3.2/src/ejercicio3.2.c
@@ -14,10 +14,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float numeroFlotante(float num);
+
 int main(void) {
 
 	setbuf(stdout, NULL);
 
+	int numero;
+
+	printf("Ingrese un número: \n");
+	scanf("%d", &numero);
+
+	printf("El número ingresado es: %.2f", numeroFlotante(numero));
 
 	return EXIT_SUCCESS;
 }
+
+float numeroFlotante(float num){
+
+	return num;
+}
+
+
