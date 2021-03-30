68b4dac76a26b136a3d5eaf0975f21ee34673a91
Jorge <jorgediazok@gmail.com>
2021-03-30 01:57:11 -0300

    ejercicio 3.4 set

---
+++ ejercicio3.4/src/ejercicio3.4.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : 4.c
+ Author      : jorgedv
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+
+Realizar un programa en donde se puedan utilizar los prototipos de la función Sumar en sus 4 combinaciones.
+int Sumar1(int, int);
+int Sumar2(void);
+void Sumar3(int, int);
+void Sumar4(void);
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	return EXIT_SUCCESS;
+}
