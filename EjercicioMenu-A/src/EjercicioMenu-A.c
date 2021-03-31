00170f28a9aaeef9418b6e512eef0c54dfae0df4
Jorge <jorgediazok@gmail.com>
2021-03-31 20:18:04 -0300

    ejercicio menu A set

---
+++ EjercicioMenu-A/src/EjercicioMenu-A.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : EjercicioMenu-A.c
+ Author      : jorgedv
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+
+ A- Hacer el menú de un programa con las siguientes opciones:
+  1. Loguearse, 2. Comprar, 3. Ver mis compras, 4. Vender, 5. Salir
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	return EXIT_SUCCESS;
+}
